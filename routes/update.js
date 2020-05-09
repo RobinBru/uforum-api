2f7ce7951be0bc0da8c8db15885fbe1f6e920836
jondsloo <jonas.desloovere@ugent.be>
2020-05-09 12:44:27 +0200

    Updated delete methods

--- routes/update.js
+++ routes/update.js
@@ -9,88 +9,106 @@ const mongoose = require('mongoose');
 
 
 router.delete('/user/:userId', function(req, res, next) {
-  User.deleteOne({ _id: req.params.userId })
-    .exec()
+	User.deleteOne({ _id: req.params.userId })
+	.exec()
     .then(result => {
-      res.send("ok");
+      return Group.updateMany({admins: req.params.userId}, {
+          $pull: {admins: req.params.userId}
+      }).exec()
     })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
+    .then(result => {
+        return Group.deleteMany({admins: []})
+            .exec()
     })
+	.then(result => {
+	  res.send("ok");
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	})
 });
 
 router.delete('/group/:groupId', function(req, res, next) {
   Group.deleteOne({ _id: req.params.groupId })
-    .exec()
-    .then(result => {
-        return User.updateMany({groups: req.params.groupId},
-            {
-                $pull: {groups: req.params.groupId}
-            })
-            .exec()
-    })
-    .then(result => {
-      res.send("ok");
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    })
+	.exec()
+	.then(result => {
+		Message.deleteMany({group: req.params.groupId})
+			.exec()
+			.then()
+			.catch(err => {
+				console.log(err);
+			});
+		return User.updateMany({groups: req.params.groupId},
+			{
+				$pull: {groups: req.params.groupId}
+			})
+			.exec()
+	})
+	.then(result => {
+	  res.send("ok");
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	})
 });
 
 router.delete('/message/:messageId', function(req, res, next) {
-  Message.deleteOne({ _id: req.params.messageId })
-    .exec()
-    .then(result => {
-      res.send("ok");
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    })
+	Message.deleteOne({ _id: req.params.messageId })
+		.exec()
+		.then(result => {
+			  return Message.deleteMany({nestedIn: req.params.messageId})
+		})
+		.then(result => {
+			res.send("ok");
+		})
+		.catch(err => {
+			res.status(400).json({ message: err.message });
+			console.log(err);
+		})
 });
 
 router.patch('/user/:userId', function(req, res, next) {
   User.updateOne({ _id: req.params.userId },
-      req.body
-    )
-    .exec()
-    .then(result => {
-      res.send("ok");
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    })
+	  req.body
+	)
+	.exec()
+	.then(result => {
+	  res.send("ok");
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	})
 });
 
 router.patch('/group/:groupId', function(req, res, next) {
   Group.updateOne({ _id: req.params.groupId },
-      req.body
-    )
-    .exec()
-    .then(result => {
-      res.send("ok");
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    })
+	  req.body
+	)
+	.exec()
+	.then(result => {
+	  res.send("ok");
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	})
 });
 
 router.patch('/message/:messageId', function(req, res, next) {
   Message.updateOne({ _id: req.params.messageId },
-      req.body
-    )
-    .exec()
-    .then(result => {
-      res.send("ok");
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    })
+	  req.body
+	)
+	.exec()
+	.then(result => {
+	  res.send("ok");
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	})
 });
 
 
@@ -99,62 +117,62 @@ router.patch('/message/:messageId/upvotes', (req, res, next) => {
   const userId = req.body.userId;
   const value = req.body.value;
   Upvote.findOne({ message: messageId, user: userId })
-    .exec()
-    .then(result => {
-      if (!result) {
-        return addUpvote(messageId, userId, value, res);
-      } else {
-        return updateUpvote(messageId, userId, result.value + value, res);
-      }
-    }).catch(err => {
-      res.status(500).json({ message: err.message });
-    console.log(err);
-    });
+	.exec()
+	.then(result => {
+	  if (!result) {
+		return addUpvote(messageId, userId, value, res);
+	  } else {
+		return updateUpvote(messageId, userId, result.value + value, res);
+	  }
+	}).catch(err => {
+	  res.status(500).json({ message: err.message });
+	console.log(err);
+	});
 
 });
 
 
 function updateUpvote(messageId, userId, value, res) {
   Upvote.findOneAndUpdate({ message: messageId, user: userId }, { user: userId, value: value })
-    .exec()
-    .then(result => {
-      if (result) {
-        res.status(200).send("ok");
-      }
-    });
+	.exec()
+	.then(result => {
+	  if (result) {
+		res.status(200).send("ok");
+	  }
+	});
 }
 
 function addUpvote(messageId, userId, value, res) {
   Message.findById(messageId)
-    .exec()
-    .then(result => {
-      if (!result) {
-        throw { message: "Unknown messageId" }
-        return;
-      }
-      User.findById(userId)
-        .exec()
-        .then(result => {
-          if (!result) {
-            throw { message: "Unknown userId" }
-          }
-          let upvote = new Upvote({
-            _id: new mongoose.Types.ObjectId(),
-            message: messageId,
-            user: userId,
-            value: value
-          });
-          upvote.save();
-          res.status(200).send("ok")
-        }).catch(err => {
-          res.status(400).json({ message: err.message });
-        console.log(err);
-        })
-    })
-    .catch(err => {
-      res.status(400).json({ message: err.message });
-    console.log(err);
-    });
+	.exec()
+	.then(result => {
+	  if (!result) {
+		throw { message: "Unknown messageId" }
+		return;
+	  }
+	  User.findById(userId)
+		.exec()
+		.then(result => {
+		  if (!result) {
+			throw { message: "Unknown userId" }
+		  }
+		  let upvote = new Upvote({
+			_id: new mongoose.Types.ObjectId(),
+			message: messageId,
+			user: userId,
+			value: value
+		  });
+		  upvote.save();
+		  res.status(200).send("ok")
+		}).catch(err => {
+		  res.status(400).json({ message: err.message });
+		console.log(err);
+		})
+	})
+	.catch(err => {
+	  res.status(400).json({ message: err.message });
+	console.log(err);
+	});
 }
 
 module.exports = router;
